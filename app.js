68a4c215b3b23bbb1280864f7071a7770db4e53b
batuhankutlu <52199113+batuhankutlu@users.noreply.github.com>
2022-05-30 18:23:57 +0300

    Front-end Login init

--- app.js
+++ app.js
@@ -2,26 +2,56 @@ const resultPage = document.querySelector("#result-page");
 const register = document.querySelector("#reg-but");
 const login = document.querySelector("#log-but");
 const ul = document.querySelector(".ourDiv");
+var isAuth;
 
-var isAuth = true;
 
-//TODO: User Authentication
-
-if (isAuth) {
-    resultPage.style.visibility = "visible";
-    ul.removeChild(login);
-    register.setAttribute("href", "index.html");
-    register.innerHTML = "Logout";
+const func = async () => {
+    fetch("http://25.33.228.221:3000/auth",{
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    }).then(res => {
+        return res.json();
+    }).then(res=>{
+        isAuth =  res.auth;
+        if(isAuth)
+        {
+            resultPage.style.visibility = "visible";
+            ul.removeChild(login);
+            register.setAttribute("href", "index.html");
+            register.innerHTML = "Logout";
+        }
+    })
 }
+
+func();
 
 async function clk() {
     if(isAuth){
         try {
-            const res = await fetch('http://25.33.228.221:3000/logout', { method: 'POST' });
-            console.log(res);
+            event.preventDefault();
+            fetch('http://25.33.228.221:3000/logout', {
+                method: 'POST',
+                mode: "cors",
+                headers: {
+                    "Access-Control-Allow-Origin" : "*"
+                }
+            }).then(response => {
+                if(response.status === 200)
+                {
+                    return response.json();
+                }
+                else{
+                    console.error("Alert Alert alert " + response.status);
+                }
+            }).then((res) => {
+                isAuth = res.auth;
+                window.location.reload();
+            })
         } catch (error) {
             console.error(error);
         }
-        isAuth = false
     }
 }
