bfc5ae2900e6fd1bf7df584fe18dc4636869c38f
Hasan Berkan Eroğlu <72980770+HBE18@users.noreply.github.com>
2022-05-30 18:22:37 +0300

    Front Connection

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { HttpCode } from '../config';
-
+import { IUserInsert } from '../models/userModels';
+const path = require('path');
 const userRouter = express.Router();
-
+var userStatus = {
+    'auth' : false
+}
 userRouter
 .route('/user')
 .get((req,res) => {
@@ -10,11 +13,25 @@ userRouter
 })
 
 userRouter
+.route('/auth')
+.get((req,res) => {
+    res.status(200).json(userStatus);
+})
+
+userRouter
 .route('/login')
 .post((req,res) => {
+    userStatus.auth = true;
+    const email = req.query.email;
+    const password = req.query.password;
+    console.log(`Email = ${email} , Password = ${password}`);
+    res.json({
+        'auth' : true,
+    });
+    console.log('Logged in!');
     // Check user credentials with middlewares (Authenticate)
     // Generate Session or Send Token to User or save user to server (Authorize)
-    res.redirect("/");
+
 })
 
 userRouter
@@ -22,14 +39,18 @@ userRouter
 .post((req,res) => {
     // Check if user exists by middleware
     // Create user and insert into db 
-    res.redirect("/login");
+    res.send('false');
 })
 
 userRouter
 .route('/logout')
 .post((req,res) => {
+    console.log('Log Out.');
+    userStatus.auth = false;
     // Check if user exists, if no redirect to login if yes delete user from server or close session or expire token
-    res.redirect('/');
+    res.json({
+        'auth' : false,
+    });
 })
 
 export default userRouter;
