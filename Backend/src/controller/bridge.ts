97e9a0bb6906e8a722c7fd87fce99d05352503b1
Hasan Berkan Eroğlu <72980770+HBE18@users.noreply.github.com>
2022-06-02 14:45:15 +0300

    back-end preprocessor part 1

---
+++ Backend/src/controller/bridge.ts
@@ -0,0 +1,21 @@
+import { Options, PythonShell } from 'python-shell';
+
+export default function runScript(keyword : string) {
+    const options : Options = {
+        mode : "text",
+        pythonPath: 'C:\\Python310\\',
+        pythonOptions: ['-u'], 
+        scriptPath: 'D:\\Ders\\GitRepos\\Project-SANLP\\',
+        args: [`${keyword}`]
+    }
+
+    PythonShell.run('main.py',options,(err,result) => {
+        if(err){
+            throw err;
+        }
+        else{
+            console.log(result);
+        }
+        
+    })
+}
--- Backend/src/routes/serverRoutes.ts
+++ Backend/src/routes/serverRoutes.ts
@@ -1,6 +1,18 @@
 import express from 'express';
 import { HttpCode } from '../config';
+import runScript from '../controller/bridge';
 
 const serverRouter = express.Router();
+
+serverRouter
+.route('/submit')
+.post((req,res) => {
+    var keyword='';
+    if(req.query.keyword == 'string'){
+        keyword = req.query.keyword;
+        runScript(keyword);
+
+    } 
+})
 
 export default serverRouter;
