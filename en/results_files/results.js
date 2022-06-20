20661a71d25463322a7118f6705d2b4dd56c96b2
batuhankutlu <52199113+batuhankutlu@users.noreply.github.com>
2022-06-20 14:12:59 +0300

    Results page done

--- en/results_files/results.js
+++ en/results_files/results.js
@@ -28,39 +28,55 @@ async function getResults() {
       return res.json();
     })
     .then(async (res) => {
-      await fetch(`http://25.33.228.221:3000/getresults:${res.id}`, {
-        method: "GET",
+      console.log(res.id);
+      await fetch(`http://25.33.228.221:3000/getresults?id=${res.id}`, {
+        method: "POST",
         mode: "cors",
         headers: {
           "Access-Control-Allow-Origin": "*"
-        },
+        }
       }).then((res) => {
         return res.json();
       }).then((res) => {
         const events = document.querySelector(".events");
         const results = res.results;
-        results.forEach(result => {
+        for(const result of results) {
           const myh1 = document.createElement("h1");
-          h1.textContent = result.keyword;
-          h1.className = "resultKeyword";
+          let p = document.createElement("p");
+          p.textContent = Date(result.timestamp);
+          const div1 = document.createElement("div");
+          myh1.textContent = result.keyword;
+          myh1.className = "resultKeyword";
           let img = document.createElement("img");
           img.alt = result.timestamp;
-          img = addSourceToImage(img, result.twitter);
+          img.title = Date(result.timestamp);
+          img.src = "data:image/png;base64," + result.twitter;
+          div1.appendChild(img);
 
+
+          const div2 = document.createElement("div");
           let img2 = document.createElement("img");
           img2.alt = result.timestamp;
-          img2 = addSourceToImage(img, result.yt_comment);
+          img2.title = Date(result.timestamp);
+          img2.src = "data:image/png;base64," + result.yt_comment;
+          div2.appendChild(img2);
 
+
+          const div3 = document.createElement("div");
           let img3 = document.createElement("img");
           img3.alt = result.timestamp;
-          img3 = addSourceToImage(img, result.yt_content);
+          img3.title = Date(result.timestamp);
+          img3.src = "data:image/png;base64," + result.yt_content;
+          div3.appendChild(img3);
 
           events.appendChild(myh1);
+          events.appendChild(document.createElement("hr"));
+          events.appendChild(p);
           events.appendChild(img);
           events.appendChild(img2);
           events.appendChild(img3);
 
-        });
+        }
       });
     });
 }
